103220c64fbb925b6efaac747ef24e18659bcb51
HP <HP@Priya1123>
2024-11-23 20:10:05 +0530

    give msg

--- practice/src/practice/Test.java
+++ practice/src/practice/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello main method");
 		System.out.println("welcome to github");
+		System.out.println("welcome to team");
 	
 	}
 
